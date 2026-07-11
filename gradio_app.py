c6d2e15f997692130a2dea39d19766f92eeeb223
yuecideng <dengyueci@qq.com>
2026-07-12 00:26:38 +0800

    wip

--- gradio_app.py
+++ gradio_app.py
@@ -52,7 +52,7 @@ from PIL import Image, ImageDraw, ImageOps
 
 
 EMBODICHAIN_ROOT = Path(
-    os.environ.get("EMBODICHAIN_ROOT", "/home/dex/桌面/EmbodiChain")
+    os.environ.get("EMBODICHAIN_ROOT", "/home/dex/workspace/sources/EmbodiChain")
 ).expanduser()
 SCENE_ID = "current"
 
